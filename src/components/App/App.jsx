afd4f34d367eea89d87049fc0d6f69c2d02734a5
Yevhenii009 <stifgun@gmail.com>
2023-03-31 13:11:33 +0300

    дз3.1реакт

---
+++ src/components/App/App.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+import 'modern-normalize';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitData = data => {
+    const normalizedName = data.name.toLowerCase();
+    const filtredContacts = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === normalizedName
+    );
+
+    if (filtredContacts) {
+      alert(`${data.name} is already in contacts.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        {
+          name: data.name,
+          number: data.number,
+          id: nanoid(),
+        },
+      ],
+    }));
+  };
+
+  handleFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const filtredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div className={css.wrapper}>
+        <h2 className={css.title}>PhoneBook</h2>
+        <ContactForm onSummit={this.formSubmitData} />
+
+        <h3>Contacts</h3>
+        <Filter value={this.state.filter} handleFilter={this.handleFilter} />
+
+        <ContactList
+          filtredContacts={filtredContacts}
+          onDelete={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
